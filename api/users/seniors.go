40199e00fedeb183814e2f89ae66d24b84c385b8
LAPTOP-0FAOULKR\axelm <axelmick15@gmail.com>
2026-03-08 15:50:20 +0100

    Correction user

--- api/users/seniors.go
+++ api/users/seniors.go
@@ -44,7 +44,7 @@ func Read_User(response http.ResponseWriter, request *http.Request) {
                a.rue, a.ville, a.code_postal, a.pays
         FROM UTILISATEUR u
         LEFT JOIN ADRESSE a ON u.id_adresse = a.id_adresse
-        WHERE u.statut = 'user'
+        WHERE u.statut = 'user' OR u.statut = 'banni'
         LIMIT ? OFFSET ?
     `
     rows, err := db.DB.Query(query, limit, offset)
